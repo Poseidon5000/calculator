40897d2fc1a39e5dab5698698544a19e5d00cb8d
Poseidon5000 <106795961+Poseidon5000@users.noreply.github.com>
2022-07-10 08:58:33 +0200

    -m "finished work"

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,12 +1,3 @@
-// const figure = document.querySelectorAll(['data-number'])
-// const actions = document.querySelectorAll(['data-operation'])
-// const clearAll = document.querySelector(['data-clear'])
-// const equal = document.querySelector(['data-equals'])
-// const deletes = document.querySelector(['data-delete'])
-// const firstDisplay = document.querySelector(['data-div1'])
-// const secondDisplay = document.querySelector(['data-div2']
-
-
 //call out parameters from HTML
 
 const firstDisplay = document.querySelector(".div1");
@@ -21,24 +12,29 @@ results.forEach(key=>{
 
 function calculate(){
    let buttonDisplay = this.innerText;
-   console.log("working");
-
+  
    if(buttonDisplay==="AC"){
-    firstDisplay.innerText = "";
+     firstDisplay.innerText = "";
     secondDisplay.innerText= "0";
     return
    }
 
-
-   if(buttonDisplay==="DEL"){
-     firstDisplay.textContent=firstDisplay.textContent.slice(0,-1)
-    // firstDisplay.textContent= firstDisplay.textContent.substr(0, firstDisplay.textContent.length-1)
+    if(buttonDisplay==="DEL"){
+      firstDisplay.textContent=firstDisplay.textContent.slice(0,-1)
+    firstDisplay.textContent= firstDisplay.textContent.substr(0, firstDisplay.textContent.length-1);
+    return
    }
 
-   if(buttonDisplay==="="){
-    secondDisplay.innerText=eval(firstDisplay.innerText)
+    if(buttonDisplay==="="){   
+       secondDisplay.innerText=eval(firstDisplay.innerText);
+      
    }
-}
+  
+
+   else{
+    firstDisplay.textContent+= buttonDisplay;
+   }
+  }
 
 
 
